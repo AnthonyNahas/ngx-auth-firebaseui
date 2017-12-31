07ff3b7ac43c82caa717d2ce34ccfb99078b357f
Anthony Nahas <anthony.na@hotmail.de>
2017-12-31 06:43:40 +0100

    Feat: enhanced the auth comonent (@google provider - sign in flow)

--- src/auth/auth.component.ts
+++ src/auth/auth.component.ts
@@ -1,7 +1,10 @@
 import {Component} from '@angular/core';
 import {AbstractControl, FormControl, Validators} from '@angular/forms';
 import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
+import {MatIconRegistry, MatSnackBar} from '@angular/material';
+import {AngularFireAuth} from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+import {ResponseSnackbarComponent} from './response/response.snackbar.component';
 
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
@@ -16,12 +19,46 @@ export class AuthComponent {
     errorMessageExample1;
     emailFormControl: AbstractControl = new FormControl('', [Validators.required, Validators.pattern(EMAIL_REGEX)]);
 
-    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
-        iconRegistry
+    constructor(public afAuth: AngularFireAuth,
+                private _iconRegistry: MatIconRegistry,
+                private _sanitizer: DomSanitizer,
+                private _snackbar: MatSnackBar) {
+        _iconRegistry
             .addSvgIcon('google',
-                sanitizer.bypassSecurityTrustResourceUrl('/assets/google.svg'))
+                _sanitizer.bypassSecurityTrustResourceUrl('/assets/google.svg'))
             .addSvgIcon('facebook',
-                sanitizer.bypassSecurityTrustResourceUrl('/assets/facebook.svg'))
+                _sanitizer.bypassSecurityTrustResourceUrl('/assets/facebook.svg'))
+    }
+
+    public async signInWithGoogle() {
+
+        this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+            .then((response) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = response.credential.accessToken;
+                // The signed-in user info.
+                var user = response.user;
+                // ...
+                console.log(response);
+                this._snackbar.openFromComponent(ResponseSnackbarComponent, {
+                    data: 'success'
+                });
+            }).catch((error) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+            console.error(error);
+            this._snackbar.open(error.message, 'OK', {
+                duration: 5000,
+                horizontalPosition: 'center',
+                verticalPosition: 'bottom'
+            });
+        });
     }
 
 }
