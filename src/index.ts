a03cef51ab98e8ce933c5c18728bc9c2e68ce4d0
Anthony Nahas <anthony.na@hotmail.de>
2017-12-31 06:43:46 +0100

    Ref: minor

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import {NgModule, ModuleWithProviders} from '@angular/core';
+import {NgModule, ModuleWithProviders, InjectionToken} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {SampleComponent} from './sample.component';
 import {SampleDirective} from './sample.directive';
 import {SamplePipe} from './sample.pipe';
 import {SampleService} from './sample.service';
 import {AuthComponent} from './auth/auth.component';
-import {MatButtonModule, MatCardModule, MatIconModule, MatInputModule, MatTabsModule} from '@angular/material';
+import {
+    MatButtonModule, MatCardModule, MatChipsModule, MatIconModule, MatInputModule, MatSnackBarModule,
+    MatTabsModule
+} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {IAuthFirebaseUIConfig} from './interfaces/config.interface';
+import {AngularFireModule, FirebaseAppConfig, FirebaseAppConfigToken, FirebaseAppName} from 'angularfire2';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+import {ResponseSnackbarComponent} from './auth/response/response.snackbar.component';
 
 export * from './sample.component';
 export * from './sample.directive';
@@ -28,26 +35,66 @@ export * from './sample.service';
         MatCardModule,
         MatInputModule,
         MatButtonModule,
-        MatIconModule
+        MatIconModule,
+        MatSnackBarModule,
+        MatChipsModule,
+        AngularFireAuthModule,
     ],
-    declarations: [
-        AuthComponent,
-        SampleComponent,
-        SampleDirective,
-        SamplePipe
-    ],
-    exports: [
-        AuthComponent,
-        SampleComponent,
-        SampleDirective,
-        SamplePipe
-    ]
+    declarations:
+        [
+            AuthComponent,
+            ResponseSnackbarComponent,
+            SampleComponent,
+            SampleDirective,
+            SamplePipe
+        ],
+    exports:
+        [
+            AuthComponent,
+            ResponseSnackbarComponent,
+            SampleComponent,
+            SampleDirective,
+            SamplePipe
+        ],
+    entryComponents:
+        [
+            ResponseSnackbarComponent
+        ]
 })
-export class NgxAuthFirebaseUIModule {
-    static forRoot(): ModuleWithProviders {
+
+
+export class NgxAuthFirebaseUIModule extends AngularFireModule {
+    static forRoot(configFactory: FirebaseAppConfig, appNameFactory?: () => string, config?: IAuthFirebaseUIConfig): ModuleWithProviders {
         return {
             ngModule: NgxAuthFirebaseUIModule,
-            providers: [SampleService]
-        };
+            providers:
+                [
+                    SampleService,
+                    {
+                        provide: FirebaseAppConfigToken,
+                        useValue: configFactory
+                    },
+                    {
+                        provide: FirebaseAppName,
+                        useFactory: appNameFactory
+                    },
+                    {
+                        provide: new InjectionToken<IAuthFirebaseUIConfig>('IAuthFirebaseUIConfig - main config'),
+                        useValue: config
+                    },
+                ],
+        }
     }
 }
+
+// export class Angularfire2WorkaroundConfigModule {
+//     static initializeApp(configFactory: () => FirebaseAppConfig, appNameFactory: () => string) {
+//         return {
+//             ngModule: AngularFireModule,
+//             providers: [
+//                 {provide: FirebaseAppConfigToken, useFactory: configFactory},
+//                 {provide: FirebaseAppName, useFactory: appNameFactory}
+//             ]
+//         };
+//     }
+// }
---
+++ src/services/config.service.ts
@@ -0,0 +1,12 @@
+// import {Injectable} from '@angular/core';
+// import {IAuthFirebaseUIConfig} from '../interfaces/config.interface';
+//
+// @Injectable()
+// export class ConfigService {
+//
+//     private _config: IAuthFirebaseUIConfig;
+//
+//     constructor(config: IAuthFirebaseUIConfig) {
+//         this._config = config;
+//     }
+// }
