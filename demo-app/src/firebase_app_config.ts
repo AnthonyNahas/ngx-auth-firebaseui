668167aceb6f8beb7e322311f08a05ffdfe41695
Anthony Nahas <anthony.na@hotmail.de>
2018-01-01 21:49:00 +0100

    Ref: added firebase app config file (demo)

---
+++ demo-app/src/firebase_app_config.ts
@@ -0,0 +1,8 @@
+export const YourFirebaseAppConfig = {
+  apiKey: 'your-firebase-apiKey',
+  authDomain: 'your-firebase-authDomain',
+  databaseURL: 'your-firebase-databaseURL',
+  projectId: 'your-firebase-projectId',
+  storageBucket: 'your-firebase-storageBucket',
+  messagingSenderId: 'your-firebase-messagingSenderId'
+};
