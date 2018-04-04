42e3e2d5c6dd48d3f7754558699d083a5b1b4ec3
anthonynahas <anthony.na@hotmail.de>
2018-04-04 20:24:33 +0200

    refactor(ngx-auth-firebaseui): translated to english + added email as input

--- src/module/components/email-confirmation/email-confirmation.component.ts
+++ src/module/components/email-confirmation/email-confirmation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'ngx-auth-firebaseui-email-confirmation',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailConfirmationComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  email: string;
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
