5a5f14ce1bf934a155e5d968c590073c757aab15
anthonynahas <anthony.na@hotmail.de>
2018-04-12 22:16:34 +0200

    demo(app): integration of ngx-material-pages for instuctions' tutorial

--- demo/src/app/home/home.module.ts
+++ demo/src/app/home/home.module.ts
@@ -11,14 +11,6 @@ import {AngularFireAuthModule} from 'angularfire2/auth';
   imports: [
     CommonModule,
     AppSharedModule,
-    NgxAuthFirebaseUIModule.forRoot({
-      apiKey: 'AIzaSyASG7KxDO2z5AH9r0jlUmwiw68Ap8kG20c',
-      authDomain: 'ngx-auth-firebaseui.firebaseapp.com',
-      databaseURL: 'https://ngx-auth-firebaseui.firebaseio.com',
-      projectId: 'ngx-auth-firebaseui',
-      storageBucket: 'ngx-auth-firebaseui.appspot.com',
-      messagingSenderId: '520699629648'
-    }),
     AngularFireAuthModule,
     HomeRoutingModule,
   ],
