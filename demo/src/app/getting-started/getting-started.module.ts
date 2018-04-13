2d43f2eccad77f88ce5410b336e75f6f9bfec603
anthonynahas <anthony.na@hotmail.de>
2018-04-13 08:42:26 +0200

    demo(app): fix reverted commit

--- demo/src/app/getting-started/getting-started.module.ts
+++ demo/src/app/getting-started/getting-started.module.ts
@@ -2,34 +2,43 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {GettingStartedComponent} from './getting-started.component';
 import {GettingStartedRoutingModule} from './getting-started-routing.module';
-import {MatCheckboxModule, MatIconModule, MatInputModule, MatSidenavModule, MatStepperModule, MatTabsModule} from '@angular/material';
+import {
+  MatCheckboxModule,
+  MatDividerModule,
+  MatIconModule,
+  MatInputModule,
+  MatSidenavModule, MatSnackBarModule,
+  MatStepperModule,
+  MatTabsModule, MatTooltipModule
+} from '@angular/material';
 import {AppSharedModule} from '../shared';
 import {DisqusModule} from 'ngx-disqus';
-import {NgxMaterialPagesComponent} from './ngx-material-pages/ngx-material-pages.component';
-import {NgxMaterialPageOutlookComponent} from './ngx-material-pages/ngx-material-page-outlook/ngx-material-page-outlook.component';
-import {NgxMaterialPageContentComponent} from './ngx-material-pages/ngx-material-page-content/ngx-material-page-content.component';
-import {NgxMaterialPageLoaderComponent} from './ngx-material-pages/ngx-material-page-loader/ngx-material-page-loader.component';
+import {NgxMaterialPagesModule} from 'ngx-material-pages';
+import {HighlightModule} from 'ngx-highlightjs';
+import {ClipboardModule} from 'ngx-clipboard';
 
 @NgModule({
   imports: [
     CommonModule,
     GettingStartedRoutingModule,
+    NgxMaterialPagesModule.forRoot(),
+    HighlightModule.forRoot(),
     DisqusModule.forRoot('ngx-auth-firebaseui'),
+    ClipboardModule,
     AppSharedModule,
     MatSidenavModule,
     MatStepperModule,
     MatCheckboxModule,
     MatInputModule,
     MatTabsModule,
-    MatIconModule
+    MatIconModule,
+    MatDividerModule,
+    MatSnackBarModule,
+    MatTooltipModule
   ],
   declarations:
     [
       GettingStartedComponent,
-      NgxMaterialPagesComponent,
-      NgxMaterialPageOutlookComponent,
-      NgxMaterialPageContentComponent,
-      NgxMaterialPageLoaderComponent,
     ],
 })
 export class GettingStartedModule {
