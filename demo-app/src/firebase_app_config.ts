f3389bd1972f9d0dc41a8d48d65322b9e9ee6e5f
anthonynahas <anthony.na@hotmail.de>
2018-04-15 22:30:19 +0200

    demo(deprecated_app): removed the deprecated demo app from project

--- demo-app/src/firebase_app_config.ts
+++
@@ -1,8 +0,0 @@
-export const YourFirebaseAppConfig = {
-  apiKey: 'your-firebase-apiKey',
-  authDomain: 'your-firebase-authDomain',
-  databaseURL: 'your-firebase-databaseURL',
-  projectId: 'your-firebase-projectId',
-  storageBucket: 'your-firebase-storageBucket',
-  messagingSenderId: 'your-firebase-messagingSenderId'
-};
