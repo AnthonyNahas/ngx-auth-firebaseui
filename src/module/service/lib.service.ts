0306b6cc6c997a4b8b473f5160bfcf05718edc37
anthonynahas <anthony.na@hotmail.de>
2018-04-22 20:29:22 +0200

    refactor(project): removed unwanted service

--- src/module/service/lib.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class LibService {
-  constructor() { }
-  sayHello(name?: String) {
-    return `Hello ${name || 'Stanger'}!`;
-  }
-}
