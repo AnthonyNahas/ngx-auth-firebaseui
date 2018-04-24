f7c56633183c6f844cb5a1f3da1f7468a6d44ba5
anthonynahas <anthony.na@hotmail.de>
2018-04-24 20:34:04 +0200

    fix(ngx-auth-firebaseui): correction of the email regex in the auth.component.ts

--- src/module/components/auth/auth.component.ts
+++ src/module/components/auth/auth.component.ts
@@ -9,10 +9,10 @@ import {Subscription} from 'rxjs/Subscription';
 import {isPlatformBrowser} from '@angular/common';
 
 
-const EMAIL_REGEX = new RegExp(['^(([^<>()\[\]\\.,;:\s@"]' +
-'+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}' +
-'\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)' +
-'+[a-zA-Z]{2,}))$'].join(''));
+const EMAIL_REGEX = new RegExp(['^(([^<>()[\\]\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\.,;:\\s@\"]+)*)',
+  '|(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.',
+  '[0-9]{1,3}\])|(([a-zA-Z\\-0-9]+\\.)+',
+  '[a-zA-Z]{2,}))$'].join(''));
 
 @Component({
   selector: 'ngx-auth-firebaseui',
