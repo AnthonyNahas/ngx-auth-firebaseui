0f3b8894bb3e56f7d69168ab9f90d87c6840b1a4
Anthony Nahas <anthony.na@hotmail.de>
2018-05-23 20:01:52 +0200

    feat(assets): added verification's email status and onAccountDeleted output event

--- src/module/components/user/user.component.ts
+++ src/module/components/user/user.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {AngularFireAuth} from 'angularfire2/auth';
 import {MatSnackBar} from '@angular/material';
+import {AuthProcessService} from '../../services/auth-process.service';
 
 @Component({
   selector: 'ngx-auth-firebaseui-user',
@@ -9,9 +10,13 @@ import {MatSnackBar} from '@angular/material';
 })
 export class UserComponent implements OnInit {
 
+  @Output()
+  onAccountDeleted: EventEmitter<void> = new EventEmitter();
+
   editMode: boolean;
 
   constructor(public auth: AngularFireAuth,
+              public authProcess: AuthProcessService,
               private snackBar: MatSnackBar) {
   }
 
@@ -24,5 +29,27 @@ export class UserComponent implements OnInit {
     // user.updateProfile()
     // user.updateEmail()
     this.snackBar.open('Sorry! This feature is under development', 'Ok');
+  }
+
+  /**
+   * Delete the account of the current firebase user
+   *
+   * On Success, emit the <onAccountDeleted> event and toast a msg!#
+   * Otherwise, log the and toast and error msg!
+   *
+   */
+  async deleteAccount() {
+    try {
+      await this.authProcess.deleteAccount();
+      this.onAccountDeleted.emit();
+      this.snackBar.open('Your account has been successfully deleted!', 'OK', {
+        duration: 5000
+      })
+    } catch (error) {
+      console.log('Error while delete user\'s account', error);
+      this.snackBar.open('Error occurred while deleting your account!', 'OK', {
+        duration: 5000
+      })
+    }
   }
 }
