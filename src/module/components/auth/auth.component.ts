f8f65cbd613ce65306b80f1f720b4e1d79852b1e
Anthony Nahas <anthony.na@hotmail.de>
2018-05-23 20:06:25 +0200

    feat(assets): added sign in as guest feature

--- src/module/components/auth/auth.component.ts
+++ src/module/components/auth/auth.component.ts
@@ -31,6 +31,7 @@ export class AuthComponent implements OnInit, OnDestroy {
   @Output()
   onError: any;
 
+  authProvider = AuthProvider;
   passwordResetWished: any;
 
   public signInFormGroup: FormGroup;
