78d92e918764fbec3eb2092768b1feae5830ca56
Anthony Nahas <anthony.na@hotmail.de>
2018-06-01 21:39:15 +0200

    feat(providers): added optional layout input | "row" or "column"

--- src/module/components/providers/auth.providers.component.ts
+++ src/module/components/providers/auth.providers.component.ts
@@ -1,6 +1,11 @@
 import {Component, Input} from '@angular/core';
 import {AuthProcessService, AuthProvider} from '../../services/auth-process.service';
 
+export enum Layout {
+  ROW = 'row',
+  COLUMN = 'column'
+}
+
 @Component({
   selector: 'ngx-auth-firebaseui-providers',
   templateUrl: 'auth.providers.component.html',
@@ -8,6 +13,9 @@ import {AuthProcessService, AuthProvider} from '../../services/auth-process.serv
 })
 
 export class AuthProvidersComponent {
+
+  @Input()
+  layout: string = Layout.ROW;
 
   @Input()
   providers: string[] | string = AuthProvider.ALL; //  google, facebook, twitter, github
