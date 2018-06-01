276db04a9e0160dab13cab5d9d6745dc211d79c2
Anthony Nahas <anthony.na@hotmail.de>
2018-06-01 21:02:55 +0200

    feat(provider): by default all providers will be available with ngx-auth-firebaseui-providers

--- src/module/components/providers/auth.providers.component.ts
+++ src/module/components/providers/auth.providers.component.ts
@@ -10,7 +10,7 @@ import {AuthProcessService, AuthProvider} from '../../services/auth-process.serv
 export class AuthProvidersComponent {
 
   @Input()
-  providers: string[] | string; //  google, facebook, twitter, github
+  providers: string[] | string = AuthProvider.ALL; //  google, facebook, twitter, github
 
   authProvider = AuthProvider;
 
