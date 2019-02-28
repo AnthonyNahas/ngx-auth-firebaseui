0d648c7564ed290db2949d15870b5b203fce283b
Anthony Nahas <anthony.na@hotmail.de>
2019-02-28 22:20:54 +0100

    feat(demo): added a registration component as example for new input #202

---
+++ demo/src/app/examples/registration/registration.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {ExampleBaseComponent} from '../example.abstract';
+import {MatSnackBar} from '@angular/material';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent extends ExampleBaseComponent {
+
+  firstTabHtml = `<ngx-auth-firebaseui  [registrationEnabled]="true"></ngx-auth-firebaseui>`;
+  secondTabHtml = `<ngx-auth-firebaseui  [registrationEnabled]="false"></ngx-auth-firebaseui>`;
+
+  constructor(public snackBar: MatSnackBar) {
+    super(snackBar);
+  }
+
+}
