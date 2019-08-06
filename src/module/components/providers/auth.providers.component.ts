3bb0351d721fa315d7052942d9705bc1b0cb0b04
Jean DAT <jean.dat@gmail.com>
2019-08-06 07:18:51 +0200

    refactor(icons): the sooner the better
    Moved declaration in module.

--- src/module/components/providers/auth.providers.component.ts
+++ src/module/components/providers/auth.providers.component.ts
@@ -1,7 +1,5 @@
-import {Component, Input, Output} from '@angular/core';
-import {DomSanitizer} from '@angular/platform-browser';
-import {MatIconRegistry} from '@angular/material';
-import {AuthProcessService, AuthProvider} from '../../services/auth-process.service';
+import { Component, Input, Output } from '@angular/core';
+import { AuthProcessService, AuthProvider } from '../../services/auth-process.service';
 
 export enum Theme {
   DEFAULT = 'default',
@@ -24,9 +22,9 @@ export enum Layout {
 })
 export class AuthProvidersComponent {
 
-  @Input() theme: string; // theme: string = Theme.DEFAULT;
+  @Input() theme: Theme; // theme: string = Theme.DEFAULT;
   @Input() layout: string = Layout.ROW;
-  @Input() providers: string[] | string = AuthProvider.ALL; //  google, facebook, twitter, github, microsoft, yahoo
+  @Input() providers: AuthProvider[] | AuthProvider = AuthProvider.ALL; //  google, facebook, twitter, github, microsoft, yahoo
 
   @Output() onSuccess: any;
   @Output() onError: any;
@@ -34,27 +32,7 @@ export class AuthProvidersComponent {
   themes = Theme;
   authProvider = AuthProvider;
 
-  constructor(public authProcess: AuthProcessService,
-              private _iconRegistry: MatIconRegistry,
-              private _sanitizer: DomSanitizer) {
-    _iconRegistry
-      .addSvgIcon('google',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/mdi/google.svg'))
-      .addSvgIcon('google-colored',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/google.svg'))
-      .addSvgIcon('facebook',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/mdi/facebook.svg'))
-      .addSvgIcon('twitter',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/mdi/twitter.svg'))
-      .addSvgIcon('github',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/mdi/github-circle.svg'))
-      .addSvgIcon('microsoft',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/mdi/microsoft.svg'))
-      .addSvgIcon('yahoo',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/mdi/yahoo.svg'))
-      .addSvgIcon('phone',
-        _sanitizer.bypassSecurityTrustResourceUrl('/assets/phone.svg'));
-
+  constructor(public authProcess: AuthProcessService) {
     this.onSuccess = authProcess.onSuccessEmitter;
     this.onError = authProcess.onErrorEmitter;
   }
