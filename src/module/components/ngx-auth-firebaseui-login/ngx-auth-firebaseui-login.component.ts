fd9ce9415cedf9193bede4657c1775d8ba1753b2
Anthony Nahas <anthony.na@hotmail.de>
2019-08-15 22:21:53 +0200

    test(package): implemented the authentication process of `ngx-auth-firebaseui-login` comp. #316

--- src/module/components/ngx-auth-firebaseui-login/ngx-auth-firebaseui-login.component.ts
+++ src/module/components/ngx-auth-firebaseui-login/ngx-auth-firebaseui-login.component.ts
@@ -1,7 +1,8 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation} from '@angular/core';
+import {Component, EventEmitter, Inject, Input, OnInit, Output, PLATFORM_ID, ViewEncapsulation} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {MatFormFieldAppearance} from '@angular/material';
-import {AuthProvider} from '../../..';
+import {AuthProcessService, AuthProvider} from '../../..';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'ngx-auth-firebaseui-login',
@@ -11,12 +12,12 @@ import {AuthProvider} from '../../..';
 })
 export class NgxAuthFirebaseuiLoginComponent implements OnInit {
 
-  logoUrl: string;
-  loginForm: FormGroup;
-
+  @Input() logoUrl: string;
   @Input() providers: string[] | string = AuthProvider.ALL; //  google, facebook, twitter, github as array or all as one single string
   @Input() appearance: MatFormFieldAppearance;
   @Input() resetPasswordEnabled = true;
+  @Input() messageOnAuthSuccess: string;
+  @Input() messageOnAuthError: string;
 
   // i18n
   @Input() titleText = 'LOGIN TO YOUR ACCOUNT';
@@ -41,14 +42,39 @@ export class NgxAuthFirebaseuiLoginComponent implements OnInit {
   @Output() onError: any;
   @Output() onCreateAccountRequested: EventEmitter<void> = new EventEmitter<void>();
 
-  constructor(private _formBuilder: FormBuilder) {
+  loginForm: FormGroup;
+  authProviders = AuthProvider;
+  onErrorSubscription: Subscription;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    public authProcess: AuthProcessService,
+    private _formBuilder: FormBuilder) {
+    this.onSuccess = authProcess.onSuccessEmitter;
+    this.onError = authProcess.onErrorEmitter;
   }
 
   ngOnInit() {
+
+    this.updateAuthSnackbarMessages();
+
     this.loginForm = this._formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
     });
   }
 
+  public updateAuthSnackbarMessages(): void {
+    this.authProcess.messageOnAuthSuccess = this.messageOnAuthSuccess;
+    this.authProcess.messageOnAuthError = this.messageOnAuthError;
+  }
+
+  async login() {
+    return await this.authProcess.signInWith(this.authProviders.EmailAndPassword,
+      {
+        email: this.loginForm.get('email').value,
+        password: this.loginForm.get('password').value
+      }
+    )
+  }
 }
