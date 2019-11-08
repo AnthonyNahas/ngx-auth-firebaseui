eed3d96e750d78464c3e81f871ea2099373ca1c7
Vinicius Guimaraes <vinicius_galvao@msn.com>
2019-11-07 22:44:31 -0800

    Towards fixing the phoneNumber field of User template...

--- src/module/components/ngx-auth-firebaseui/auth.component.ts
+++ src/module/components/ngx-auth-firebaseui/auth.component.ts
@@ -36,7 +36,7 @@ export const EMAIL_REGEX = new RegExp(['^(([^<>()[\\]\\\.,;:\\s@\"]+(\\.[^<>()\\
   '[0-9]{1,3}\])|(([a-zA-Z\\-0-9]+\\.)+',
   '[a-zA-Z]{2,}))$'].join(''));
 
-export const PHONE_NUMBER_REGEX = new RegExp(/^\+(?:[0-9] ?){6,14}[0-9]$/);
+export const PHONE_NUMBER_REGEX = new RegExp(['^[+]{0,1}[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\.]{0,1}[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]{4,12}$'].join(''));
 
 @Component({
   selector: 'ngx-auth-firebaseui',
