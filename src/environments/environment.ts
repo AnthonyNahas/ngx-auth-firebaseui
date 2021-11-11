05bd54ca593fe3f1187f3cfc9538835ca83619c3
Chris Curnow <chris@4cast.com.au>
2021-11-11 15:25:13 +1100

    Remove personal firebase config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,26 +2,12 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+export const environment = {
+  production: false,
+  config: {}
+};
 
-const config = {
-    apiKey: 'AIzaSyDDC8OlxiOrUK7dokf0Ad12dX3vorS6wag',
-    authDomain: 'fourcastpro.firebaseapp.com',
-    databaseURL: 'https://fourcastpro.firebaseio.com',
-    projectId: 'fourcastpro',
-    storageBucket: 'fourcastpro.appspot.com',
-    messagingSenderId: '346034054648'
-    };
-
-  export const environment = {
-    envName: 'dev',
-    production: false,
-    standalone: true,
-    is4dHost: false,
-    serverUrl: 'http://localhost:8081',
-    projectId: '4DCD3CD18C68456388537415CE4BE594',
-    firebaseConfig: config
-  };
-
+// YOU MUST PROVIDE YOUR OWN FIREBASE CONFIG AS config ABOVE
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -29,4 +15,4 @@ const config = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
